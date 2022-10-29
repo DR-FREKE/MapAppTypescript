1cfe48c06094814829288a16a39817a15015e1dd
Solomon Ndifreke <solomonndifreke@GDLs-MacBook-Pro.local>
2022-10-29 14:37:05 +0100

    marker commit

--- src/Company.ts
+++ src/Company.ts
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker";
 import { LocationType } from "../types";
 import { Mappable } from "./MapInterface";
 
-export class Company {
+export class Company implements Mappable {
   name: string;
   catchPhrase: string;
   location: LocationType;
